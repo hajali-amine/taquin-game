921c7e28790256247f5783f4a1822e0ef286ce0f
hajali-amine <amine.hajali@insat.ucar.tn>
2022-02-28 17:41:26 +0100

    typing

--- taquin.py
+++ taquin.py
@@ -1,59 +1,62 @@
 import random
 from copy import copy, deepcopy
+from typing import final
+from typing_extensions import Self
 
 
 class Taquin:
 
     def __init__(self):
-        self.n = 3  # Board's height and width
-        self.depth = 0
+        self.n: int = 3  # Board's height and width
+        self.depth: int = 0
         # Instantiating the Taquin with random values
-        self.board_numbers = {}
-        coordinates_list = [(i, j) for i in range(self.n) for j in range(self.n)]
+        self.board_numbers: dict[int, int] = {}
+        coordinates_list: list[tuple(int, int)] = [(i, j) for i in range(self.n) for j in range(self.n)]
         random.shuffle(coordinates_list)
 
         # Add the numbers with random coordinates
         for i in range(self.n ** 2 - 1):
             self.board_numbers[coordinates_list[i]] = i + 1
-        self.empty_slot = coordinates_list[self.n ** 2 - 1]
+        self.empty_slot: tuple(int, int) = coordinates_list[self.n ** 2 - 1]
         self.board_numbers[self.empty_slot] = 0
 
-    def __str__(self):
+    def __str__(self) -> str:
         # TODO: find a better way to visualize this
-        res = " "
+        res: list[str] = [" "]
         for j in range(self.n):
             for i in range(self.n):
                 if self.empty_slot != (i, j):
-                    res = res + " " + str(self.board_numbers[(i, j)]) + " "
+                    res.append(" " + str(self.board_numbers[(i, j)]) + " ")
                 else:
-                    res = res + " * "
-            res = res + " \n "
-        return res
+                    res.append(" * ")
+            res.append(" \n ")
+        taquin_str: str = "".join(res)
+        return taquin_str
 
-    def __hash__(self):
-        taquin_hash = 0
-        ten_multiple = 1
+    def __hash__(self) -> int:
+        taquin_hash: int = 0
+        ten_multiple: int = 1
         for j in range(self.n):
             for i in range(self.n):
                 taquin_hash = taquin_hash + self.board_numbers[(i, j)] * ten_multiple
                 ten_multiple = ten_multiple * 10
         return taquin_hash
 
-    def __eq__(self, other):
+    def __eq__(self, other: Self) -> bool:
         if isinstance(other, self.__class__):
             return hash(other) == hash(self)
         else:
             return False
 
-    def __ne__(self, other):
+    def __ne__(self, other: Self) -> bool:
         return not self.__eq__(other)
 
-    def init_state(self):
+    def init_state(self) -> Self:
         print("Use '*' for the empty slot")
 
         for j in range(self.n):
             print("Insert the ", self.n, "digits of line ", j + 1, ":")
-            str_values = input().split(" ")
+            str_values: list[str] = input().split(" ")
             for i in range(self.n):
                 if str_values[i] == "*":
                     self.empty_slot = (i, j)
@@ -64,37 +67,42 @@ class Taquin:
         if len(self.board_numbers.values()) != len(set(self.board_numbers.values())):
             self.init_state()
 
-    def final_state(self):
-        return hash(self) == 876543210
+    def final_state(self) -> bool:
+        final_state: int = 0
+        for i in range(1, self.n ** 2 + 1):
+            final_state *= 10
+            final_state += self.n ** 2 - i
+        print(final_state)
+        return hash(self) == final_state
 
-    def move_piece(self, new_empty_slot):
-        new_taquin = deepcopy(self)
+    def move_piece(self, new_empty_slot) -> Self:
+        new_taquin: Self = deepcopy(self)
         new_taquin.depth = new_taquin.depth + 1
         new_taquin.board_numbers[new_taquin.empty_slot] = new_taquin.board_numbers[new_empty_slot]
         new_taquin.board_numbers[new_empty_slot] = 0
         new_taquin.empty_slot = new_empty_slot
         return new_taquin
 
-    def can_move_left(self):
+    def can_move_left(self) -> Self:
         return self.empty_slot[0] < self.n - 1
 
-    def move_left(self):
+    def move_left(self) -> Self:
         return self.move_piece((self.empty_slot[0] + 1, self.empty_slot[1]))
 
-    def can_move_right(self):
+    def can_move_right(self) -> Self:
         return self.empty_slot[0] > 0
 
-    def move_right(self):
+    def move_right(self) -> Self:
         return self.move_piece((self.empty_slot[0] - 1, self.empty_slot[1]))
 
-    def can_move_up(self):
+    def can_move_up(self) -> Self:
         return self.empty_slot[1] < self.n - 1
 
-    def move_up(self):
+    def move_up(self) -> Self:
         return self.move_piece((self.empty_slot[0], self.empty_slot[1] + 1))
 
-    def can_move_down(self):
+    def can_move_down(self) -> Self:
         return self.empty_slot[1] > 0
 
-    def move_down(self):
+    def move_down(self) -> Self:
         return self.move_piece((self.empty_slot[0], self.empty_slot[1] - 1))
